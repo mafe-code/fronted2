aa0e657d8a769a450fd8688975db40f21b424c38
mafe correa <qcorreamafe591@gmail.com>
2026-08-01 17:11:28 -0500

    Initial commit

---
+++ app-sgc/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+         <div className ="m-4 flex items-center justify-center" > 
+          <h1 className ="tex-5xl text-red-800">
+             Dashboard 
+             </h1>
+          </div>
+    </>
+  )
+}
+export default App
